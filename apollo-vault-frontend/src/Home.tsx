2a5d61e90b7b1f73b74205dbcbbb16dfc1af1315
khedekar <64322243+titan-6@users.noreply.github.com>
2024-01-28 00:06:00 -0500

    Small changes to ui

--- apollo-vault-frontend/src/Home.tsx
+++ apollo-vault-frontend/src/Home.tsx
@@ -5,12 +5,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import detectEthereumProvider from '@metamask/detect-provider'
 import ApolloVault from './abis/apollo-vault.json'
-const ethers = require('ethers');
+import { ethers } from 'ethers';
 let injectedProvider = false
 
 const contractAddress = "0xb8C825e2D81583ddA938Fca7816Ab8F88e7c24c2"
-const signer = await ethers.getSigners();
-const contract = new ethers.Contract(contractAddress, ApolloVault, signer)
+
+
 
 function Homepage() {
 
@@ -65,7 +65,6 @@ function Homepage() {
             <h1 id='title'>Welcome to the Apollo Vault</h1>
             <button id="Start" type="button">Click here to start</button>
 
-            <h2>Injected Provider { injectedProvider ? 'DOES' : 'DOES NOT'} Exist</h2>
             { hasProvider &&                               
             <button onClick={handleConnect}>Connect MetaMask</button>
             }
@@ -73,27 +72,6 @@ function Homepage() {
             <div>Wallet Accounts: { wallet.accounts[0] }</div>
             }
         </div>
-        <div>
-            <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-            </button>
-            <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-        </div>
-        <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-        </p>
-        
     </>
     )
 }
