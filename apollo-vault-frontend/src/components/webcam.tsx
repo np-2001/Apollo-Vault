250b2a2a771e7d1e89369f5a9a144d44a088eaed
polavar7 <polavar7@msu.edu>
2024-01-28 11:42:55 -0500

    Fixed issue

--- apollo-vault-frontend/src/components/webcam.tsx
+++ apollo-vault-frontend/src/components/webcam.tsx
@@ -31,8 +31,7 @@ const WebcamCapture: React.FC = () => {
                         navigate('/HomeMainDisplay', {state: {name: res.data}});
                         window.sessionStorage.setItem("name", JSON.stringify(res.data))
                     }
-                    window.sessionStorage.setItem("name", JSON.stringify(res.data))
-                    navigate('/HomeMainDisplay', {state: {name: res.data}});
+
                 })
                 .catch((error: any) => {
                     console.error('Error:', error);
